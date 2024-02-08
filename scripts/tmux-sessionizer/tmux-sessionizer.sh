117b2153215a449268eddc4cfcdf774fd6581f23
I.P <ilia.yavorov.petrov@gmail.com>
2024-02-08 18:41:37 +0200

    use nvim . instead of clear

--- scripts/tmux-sessionizer/tmux-sessionizer.sh
+++ scripts/tmux-sessionizer/tmux-sessionizer.sh
@@ -5,81 +5,81 @@ pre_attach_action() {
 
 	if [[ "$1" == "common_dotfiles" ]]; then
 		cmd="cd '$COMMON/.dotfiles/' \
-        && clear"
+        && nvim ."
 	elif [[ "$1" == "common_notes" ]]; then
 		cmd="cd '$COMMON/notes/' \
-        && clear"
+        && nvim ."
 	elif [[ "$1" == "common_ledger" ]]; then
 		cmd="cd '$COMMON/ledger/' \
-        && clear"
+        && nvim ."
 	elif [[ "$1" == "work_bpa_master_data_api_feature_1" ]]; then
 		cmd="cd '$BPA/master-data-api' \
         && git checkout local -- src/main/java/com/iteconomics/bpa/masterdata/config/SecurityConfig.java > /dev/null 2>&1 \
         && git checkout feature-1 -- src/main/resources/application-local.properties > /dev/null 2>&1 \
-        && clear"
+        && nvim ."
 	elif [[ "$1" == "work_bpa_master_data_api_dev" ]]; then
 		cmd="cd '$BPA/master-data-api' \
-        && git checkout local -- src/main/java/com/iteconomics/bpa/masterdata/config/SecurityConfig.java > /dev/null 2>&1 \
+        && git checkout develop -- src/main/java/com/iteconomics/bpa/masterdata/config/SecurityConfig.java > /dev/null 2>&1 \
         && git checkout develop -- src/main/resources/application-local.properties > /dev/null 2>&1 \
-        && clear"
+        && nvim ."
 	elif [[ "$1" == "work_bpa_master_data_api_test" ]]; then
 		cmd="cd '$BPA/master-data-api' \
         && git checkout local -- src/main/java/com/iteconomics/bpa/masterdata/config/SecurityConfig.java > /dev/null 2>&1 \
         && git checkout test -- src/main/resources/application-local.properties > /dev/null 2>&1 \
-        && clear"
+        && nvim ."
 	elif [[ "$1" == "work_bpa_master_data_api_staging" ]]; then
 		cmd="cd '$BPA/master-data-api' \
         && git checkout local -- src/main/java/com/iteconomics/bpa/masterdata/config/SecurityConfig.java > /dev/null 2>&1 \
         && git checkout staging -- src/main/resources/application-local.properties > /dev/null 2>&1 \
-        && clear"
+        && nvim ."
 		# issue with fe
 	elif [[ "$1" == "work_bpa_master_data_fe_local" ]]; then
 		cmd="cd '$BPA/master-data-fe/' \
         && git checkout local -- BTP_FE/src/environments/environment.ts > /dev/null 2>&1 \
-        && clear"
+        && nvim ."
 	elif [[ "$1" == "work_bpa_master_data_fe_feature_1" ]]; then
 		cmd="cd '$BPA/master-data-fe/' \
         && git checkout feature-1 -- BTP_FE/src/environments/environment.ts > /dev/null 2>&1 \
-        && clear"
+        && nvim ."
 	elif [[ "$1" == "work_bpa_master_data_fe_dev" ]]; then
 		cmd="cd '$BPA/master-data-fe/' \
         && git checkout develop -- BTP_FE/src/environments/environment.ts > /dev/null 2>&1 \
-        && clear"
+        && nvim ."
 	elif [[ "$1" == "work_bpa_master_data_fe_test" ]]; then
 		cmd="cd '$BPA/master-data-fe/' \
         && git checkout test -- BTP_FE/src/environments/environment.ts > /dev/null 2>&1 \
-        && clear"
+        && nvim ."
 	elif [[ "$1" == "work_bpa_master_data_fe_staging" ]]; then
 		cmd="cd '$BPA/master-data-fe/' \
         && git checkout staging -- BTP_FE/src/environments/environment.ts > /dev/null 2>&1 \
-        && clear"
+        && nvim ."
 	elif [[ "$1" == "work_bpa_btp_build_image" ]]; then
 		cmd="cd '$BPA/btp_build_image/' \
-        && clear"
+        && nvim ."
 	elif [[ "$1" == "work_bpa_bpa_dev_tools" ]]; then
 		cmd="cd '$BPA/bpa-dev-tools/' \
-        && clear"
+        && nvim ."
 	elif [[ "$1" == "work_bpa_bpa_testing" ]]; then
 		cmd="cd '$BPA/bpa-testing/' \
-        && clear"
+        && nvim ."
 	elif [[ "$1" == "personal_foo" ]]; then
 		cmd="cd '$PERSONAL/foo/' \
-        && clear"
+        && nvim ."
 	elif [[ "$1" == "personal_mini_shop" ]]; then
 		cmd="cd '$PERSONAL/mini-shop/' \
-        && clear"
+        && nvim ."
 	elif [[ "$1" == "personal_mini_shop_client" ]]; then
 		cmd="cd '$PERSONAL/mini-shop-client/' \
-        && clear"
+        && nvim ."
 	elif [[ "$1" == "personal_api_gateway" ]]; then
 		cmd="cd '$PERSONAL/api-gateway/' \
-        && clear"
+        && nvim ."
 	elif [[ "$1" == "personal_go_calc" ]]; then
 		cmd="cd '$PERSONAL/go-calc/' \
-        && clear"
+        && nvim ."
 	elif [[ "$1" == "personal_flink" ]]; then
 		cmd="cd '$PERSONAL/flink/' \
-        && clear"
+        && nvim ."
 	fi
 
 	tmux send-keys -t "$2:0" "$cmd" C-m
