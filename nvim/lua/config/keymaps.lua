a32a2e101ebcd52c07ef2a39b262a75130fe79f7
Ilia Petrov <72142537+iypetrov@users.noreply.github.com>
2024-02-13 12:20:35 +0200

    Tmux sessioniser refactor (#13)
    * split tmux sessiopizer to 2 scripts
    
    * update the tmux set up

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,8 +1,8 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
-vim.keymap.set('n','y','"+y')
-vim.keymap.set('n','yy','"+yy')
-vim.keymap.set('n','Y','"+Y')
-vim.keymap.set('x','y','"+y')
-vim.keymap.set('x','Y','"+Y')
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
