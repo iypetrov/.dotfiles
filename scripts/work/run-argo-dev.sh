93dce34932eecde1c1dda2ad6d2be8effeaf0043
Ilia Petrov <ilia.yavorov.petrov@gmail.com>
2024-10-18 10:29:31 +0300

    adjust argo port

--- scripts/work/run-argo-dev.sh
+++ scripts/work/run-argo-dev.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-boundary connect -target-id ttcp_3KvICN1Eze -listen-port 8091 & \
+USER_ID="u_Uq6acX62iM"
+boundary sessions list -format "json" -recursive -filter='"/item/user_id" == "'"$USER_ID"'"' | jq -r 'if .items != null then .items[].id else empty end' | xargs -I {} boundary sessions cancel -id {}
+
+boundary connect -target-id ttcp_3KvICN1Eze -listen-port 8080
