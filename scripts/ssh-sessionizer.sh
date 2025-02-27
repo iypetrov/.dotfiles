74582e449f73f85c43492452101c6dc4387f8d7e
Ilia Petrov <ilia.yavorov.petrov@gmail.com>
2025-02-27 13:49:08 +0200

    extend ssh sessionizer

--- scripts/ssh-sessionizer.sh
+++ scripts/ssh-sessionizer.sh
@@ -2,7 +2,7 @@
 
 [[ ! $(command -v fzf) ]] && echo "Error: You need to have fzf installed" >&2 && return 1
 
-target="$(echo "access.gas-x.de ip812" | tr ' ' '\n' | fzf --tac)"
+target="$(echo "access.gas-x.de de-gasx-aws de-lab52 de-lab12 ip812" | tr ' ' '\n' | fzf --tac)"
 if [[ -z "${target}" ]]; then
   exit 1
 fi
@@ -11,18 +11,32 @@ case "${target}" in
   "access.gas-x.de")
     ssh -i ~/.ssh/id_ed25519_gasx ipetrov@access.gas-x.de
     ;;
-  "ip812")
-    while read -r instance_id; do
-        aws ssm start-session \
-            --target ${instance_id} \
+  "de-gasx-aws")
+    instance_id=$(aws ec2 describe-instances \
             --region eu-central-1 \
-            --document-name AWS-StartInteractiveCommand \
-            --parameters '{"command": ["tmux attach-session -t default || tmux new-session -s default"]}' >/dev/tty 2>/dev/tty </dev/tty
-    done < <(aws ec2 describe-instances \
-            --region eu-central-1 \
-            --filters "Name=tag:Environment,Values=prod" "Name=tag:Organization,Values=ip812" "Name=instance-state-name,Values=running" \
+            --filters "Name=instance-state-name,Values=running" \
             --query "Reservations[].Instances[].InstanceId" \
-            --output json | jq -r '.[]' | head -n 1)
+            --output json \
+            --profile 034013843855_ipgx-infra-integration | \
+            jq -r '.[]' | fzf --tac)
+
+    aws ssm start-session \
+        --target "${instance_id}" \
+        --region eu-central-1 \
+        --profile 034013843855_ipgx-infra-integration
+    ;;
+  "ip812")
+    instance_id=$(aws ec2 describe-instances \
+            --region eu-central-1 \
+            --filters "Name=instance-state-name,Values=running" \
+            --query "Reservations[].Instances[].InstanceId" \
+            --output json | jq -r '.[]' | fzf --tac)
+
+    aws ssm start-session \
+        --target ${instance_id} \
+        --region eu-central-1 \
+        --document-name AWS-StartInteractiveCommand \
+        --parameters '{"command": ["tmux attach-session -t default || tmux new-session -s default"]}' >/dev/tty 2>/dev/tty </dev/tty
     ;;
   *)
     echo "Something went wrong" >&2
