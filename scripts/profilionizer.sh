45579b85ab87bbce1e40011435aae60618226922
Ilia Petrov <ilia.yavorov.petrov@gmail.com>
2025-02-27 12:31:56 +0200

    add profilzer

---
+++ scripts/profilionizer.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+[[ ! $(command -v fzf) ]] && echo "Error: You need to have fzf installed" >&2 && return 1
+[[ ! $(command -v kubectx) ]] && echo "Error: You need to have kubectx installed" >&2 && return 1
+[[ ! $(command -v aws) ]] && echo "Error: You need to have k9s installed" >&2 && return 1
+
+targets="$(echo "kubens kubectx aws" | tr ' ' '\n' | fzf --tac)"
+if [[ -z "${targets}" ]]; then
+  exit 1
+fi
+
+case "${targets}" in
+  "kubens")
+    kubens
+    ;;
+  "kubectx")
+    kubectx
+    ;;
+  "aws")
+    acc="$(aws --no-cli-pager configure list-profiles 2>/dev/null | fzf)"
+    sed -i '' "s|export AWS_DEFAULT_PROFILE=[^ ]*|export AWS_DEFAULT_PROFILE=$acc|g" /Users/ipetrov/Documents/projects/common/.dotfiles/.zshrc
+    ;;
+
+  *)
+    echo "Something went wrong" >&2
+    ;;
+esac
+
