54c84ba18e7e07eb6f4f9d2a76a9ea5ab0f1396e
Ilia Petrov <ilia.yavorov.petrov@gmail.com>
2025-03-14 10:08:55 +0200

    ssh

--- scripts/ssh-sessionizer.sh
+++ scripts/ssh-sessionizer.sh
@@ -2,7 +2,7 @@
 
 [[ ! $(command -v fzf) ]] && echo "Error: You need to have fzf installed" >&2 && return 1
 
-target="$(echo "access.gas-x.de de-gasx-aws de-lab12 de-lab52 de-lab09 ip812" | tr ' ' '\n' | fzf --tac)"
+target="$(echo "access.gas-x.de aws" | tr ' ' '\n' | fzf --tac)"
 if [[ -z "${target}" ]]; then
   exit 1
 fi
@@ -11,84 +11,25 @@ case "${target}" in
   "access.gas-x.de")
     ssh -i ~/.ssh/id_ed25519_gasx ipetrov@access.gas-x.de
     ;;
-  "de-gasx-aws")
-    acc="de_gasx"
-    echo "${acc}" > ~/.aws/current_acc
+  "aws")
     instance_id=$(aws ec2 describe-instances \
         --region eu-central-1 \
-        --filters "Name=instance-state-name,Values=running" \
-        --query "Reservations[].Instances[].{ID: Instances[].InstanceId, Name: Instances[].Tags[?Key=='Name'].Value | [0]}" \
-        --output json \
-        --profile de_gasx | \
-        jq -r '.[] | .Name[0] + " " + .ID[0]' | fzf --tac | awk '{print $2}')
-
-    aws ssm start-session \
-        --target "${instance_id}" \
-        --region eu-central-1 \
-        --profile 034013843855_ipgx-infra-integration
-    ;;
-  "de-lab12")
-    acc="de_lab12"
-    echo "${acc}" > ~/.aws/current_acc
-    instance_id=$(aws ec2 describe-instances \
-            --region eu-central-1 \
-            --filters "Name=instance-state-name,Values=running" \
-            --query "Reservations[].Instances[].InstanceId" \
-            --output json \
-            --profile de_lab12 | \
-            jq -r '.[]' | fzf --tac)
-
-    aws ssm start-session \
-        --target "${instance_id}" \
-        --region eu-central-1 \
-        --profile 903999197368_ipgx-infra-acceptance
-    ;;
-  "de-lab52")
-    acc="de_lab52"
-    echo "${acc}" > ~/.aws/current_acc
-    instance_id=$(aws ec2 describe-instances \
-            --region eu-central-1 \
-            --filters "Name=instance-state-name,Values=running" \
-            --query "Reservations[].Instances[].InstanceId" \
-            --output json \
-            --profile de_lab52 | \
-            jq -r '.[]' | fzf --tac)
-
-    aws ssm start-session \
-        --target "${instance_id}" \
-        --region eu-central-1 \
-        --profile 833704146350_ipgx-infra-rnd
-    ;;
-  "de-lab09")
-    acc="de_lab09"
-    echo "${acc}" > ~/.aws/current_acc
-    instance_id=$(aws ec2 describe-instances \
-            --region eu-central-1 \
-            --filters "Name=instance-state-name,Values=running" \
-            --query "Reservations[].Instances[].InstanceId" \
-            --output json \
-            --profile de_lab09 | \
-            jq -r '.[]' | fzf --tac)
-
-    aws ssm start-session \
-        --target "${instance_id}" \
-        --region eu-central-1 \
-        --profile 833704146350_ipgx-infra-rnd
-    ;;
-  "ip812")
-    acc="personal"
-    echo "${acc}" > ~/.aws/current_acc
-    instance_id=$(aws ec2 describe-instances \
-            --region eu-central-1 \
-            --filters "Name=instance-state-name,Values=running" \
-            --query "Reservations[].Instances[].InstanceId" \
-            --output json | jq -r '.[]' | fzf --tac)
-
-    aws ssm start-session \
-        --target ${instance_id} \
-        --region eu-central-1 \
-        --document-name AWS-StartInteractiveCommand \
-        --parameters '{"command": ["tmux attach-session -t default || tmux new-session -s default"]}' >/dev/tty 2>/dev/tty </dev/tty
+        --query "Reservations[].Instances[]" \
+        --output json | \
+        jq -r '.[] | select(.State.Name == "running") | .InstanceId + " " + (.Tags[] | select(.Key == "Name") | .Value)' | \
+        fzf --tac | \
+        awk '{print $1}')
+    
+    if [ -n "$instance_id" ]; then
+        aws ssm start-session \
+            --target "${instance_id}" \
+            --region eu-central-1
+    fi
+# aws ssm start-session \
+    #     --target ${instance_id} \
+    #     --region eu-central-1 \
+    #     --document-name AWS-StartInteractiveCommand \
+    #     --parameters '{"command": ["tmux attach-session -t default || tmux new-session -s default"]}' >/dev/tty 2>/dev/tty </dev/tty
     ;;
   *)
     echo "Something went wrong" >&2
