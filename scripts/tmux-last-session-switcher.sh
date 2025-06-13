095573fd82a350a8c6fa0cfc5b59f6fede888b58
iypetrov <ilia.yavorov.petrov@gmail.com>
2025-06-13 09:56:21 +0300

    adjust scripts
    Signed-off-by: iypetrov <ilia.yavorov.petrov@gmail.com>

--- scripts/tmux-last-session-switcher.sh
+++ scripts/tmux-last-session-switcher.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+USERNAME="ipetrov"
 curr_session="$(tmux display-message -p '#S')"
-last_session_file="/home/ipetrov/.tmux/last_session"
+last_session_file="/home/$USERNAME/.tmux/last_session"
 last_session="$(cat "${last_session_file}")"
 
 tmux switch-client -t "${last_session}"
--- scripts/tmux-sessionizer.sh
+++ scripts/tmux-sessionizer.sh
@@ -2,9 +2,10 @@
 
 [[ ! $(command -v fzf) ]] && echo "Error: You need to have fzf installed" >&2 && return 1
 
-last_session_file="/home/ipetrov/.tmux/last_session"
+USERNAME="ipetrov"
+last_session_file="/home/$USERNAME/.tmux/last_session"
 
-target="$(find "/home/ipetrov/projects/common" "/home/ipetrov/projects/personal" "/home/ipetrov/projects/ip812" "/home/ipetrov/projects/avalon" "/home/ipetrov/projects/work" -mindepth 1 -maxdepth 1 -type d | fzf)"
+target="$(find "/home/$USERNAME/projects/common" "/home/$USERNAME/projects/personal" "/home/$USERNAME/projects/ip812" "/home/$USERNAME/projects/avalon" "/home/$USERNAME/projects/work" -mindepth 1 -maxdepth 1 -type d | fzf)"
 if [[ -z $"{target}" ]]; then
     exit 0
 fi
