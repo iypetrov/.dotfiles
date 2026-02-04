1775055985e9870c136e93a16d78759d50ef044b
Ilia Petrov <ilia.yavorov.petrov@gmail.com>
2026-02-04 08:51:36 +0200

    fix: make check if last session exists on tmux-last-session-switcher.sh
    Signed-off-by: Ilia Petrov <ilia.yavorov.petrov@gmail.com>

--- scripts/tmux-last-session-switcher.sh
+++ scripts/tmux-last-session-switcher.sh
@@ -4,5 +4,8 @@ curr_session="$(tmux display-message -p '#S')"
 last_session_file="/root/.tmux/last_session"
 last_session="$(cat "${last_session_file}")"
 
-tmux switch-client -t "${last_session}"
+if [[ ! -z "${last_session}" ]]; then
+    tmux switch-client -t "${last_session}"
+fi
+
 echo "${curr_session}" > "${last_session_file}"
