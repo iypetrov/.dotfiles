63b50801c33e87c31ca870011c48c8e52e2abe35
Ilia Petrov <ilia.yavorov.petrov@gmail.com>
2026-02-23 17:36:08 +0200

    feat: rm not needed options from profilionizer
    Signed-off-by: Ilia Petrov <ilia.yavorov.petrov@gmail.com>

--- scripts/profilionizer.sh
+++ scripts/profilionizer.sh
@@ -2,9 +2,8 @@
 
 [[ ! $(command -v fzf) ]] && echo "Error: You need to have fzf installed" >&2 && return 1
 [[ ! $(command -v kubectx) ]] && echo "Error: You need to have kubectx installed" >&2 && return 1
-[[ ! $(command -v aws) ]] && echo "Error: You need to have k9s installed" >&2 && return 1
 
-targets="$(echo "python kubens kubectx aws" | tr ' ' '\n' | fzf --tac)"
+targets="$(echo "kubens kubectx" | tr ' ' '\n' | fzf --tac)"
 if [[ -z "${targets}" ]]; then
   exit 1
 fi
@@ -15,9 +14,6 @@ case "${targets}" in
     ;;
   "kubectx")
     kubectx
-    ;;
-  "aws")
-    aws configure sso
     ;;
 
   *)
